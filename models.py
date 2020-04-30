370ed6020c4ce49eddebdc788c0878d80b088a04
Steve Boyer <stephen.a.boyer@gmail.com>
2020-04-30 13:22:57 -0500

    created taqueria table

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from peewee import *
-
+import datetime
 
 
 DATABASE = SqliteDatabase('taquerias.sqlite')
@@ -7,19 +7,29 @@ DATABASE = SqliteDatabase('taquerias.sqlite')
 
 
 class User(Model):
-  username=CharField(unique=True)
-  email=CharField(unique=True)
-  password=CharField() 
+  username = CharField(unique=True)
+  email = CharField(unique=True)
+  password = CharField() 
 
   class Meta: 
     database=DATABASE
 
+class Taqueria(Model):
+  name = CharField()
+  patron_id = IntegerField()
+  address = CharField()
+  zip_code = IntegerField()
+  rating = IntegerField()
+  recommendations = CharField()
+  created_at = DateTimeField(default=datetime.datetime.now)
 
+  class Meta:
+    database = DATABASE
 
 
 def initialize():
   DATABASE.connect()
-  DATABASE.create_tables([User], safe=True)
+  DATABASE.create_tables([User, Taqueria], safe=True)
   print("Connected to DB and created tables if they weren't already there")
 
   DATABASE.close()
