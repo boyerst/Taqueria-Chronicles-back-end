bd238564a6ad697505cde841bcbeb7ea3ae4aa62
Steve Boyer <stephen.a.boyer@gmail.com>
2020-04-30 15:09:21 -0500

    added login route, login route hitting

--- resources/users.py
+++ resources/users.py
@@ -4,6 +4,8 @@ from flask_bcrypt import generate_password_hash, check_password_hash
 
 from playhouse.shortcuts import model_to_dict
 
+from flask_login import login_user
+
 
 users = Blueprint('users', 'users')
 
@@ -17,7 +19,6 @@ def test_user_resource():
 @users.route('/register', methods=['POST'])
 def register():
   payload = request.get_json()
-  # print(payload)
   payload['username'] = payload['username'].lower()
   payload['email'] = payload['email'].lower()
   print(payload)
@@ -30,7 +31,6 @@ def register():
       status=401
     ), 401
 
-
   except models.DoesNotExist:
     created_user = models.User.create(
       username=payload['username'],
@@ -38,13 +38,28 @@ def register():
       password=generate_password_hash(payload['password'])
     )
     print(created_user)
+    login_user(created_user) #NOTE: takes user OBJ and logs them in/starts session
     created_user_dict = model_to_dict(created_user)
     print(type(created_user_dict['password']))
     created_user_dict.pop('password')
 
-  
     return jsonify(
         data=created_user_dict,
         message=f"Welcome! You have successfully registered user {created_user_dict['email']}",
         status=201
       ), 201
+
+
+# LOGIN /users/login
+@users.route('/login', methods=['POST'])
+def login():
+  payload = request.get_json()
+  payload['email'] = payload['email'].lower()
+  payload['username'] = payload['username'].lower()
+  return "check terminal"
+
+
+
+
+
+
