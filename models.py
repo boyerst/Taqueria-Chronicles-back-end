2a9832910380fa5a69024c92620e2c14235c83ce
Steve Boyer <stephen.a.boyer@gmail.com>
2020-04-30 13:53:33 -0500

    imported UserMixin, added to User class

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 from peewee import *
 import datetime
 
+from flask_login import UserMixin
 
 DATABASE = SqliteDatabase('taquerias.sqlite')
 
 
 
-class User(Model):
+class User(UserMixin, Model):
   username = CharField(unique=True)
   email = CharField(unique=True)
   password = CharField() 
