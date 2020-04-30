c3193f97526775241598761b69bce2a493ca676a
Steve Boyer <stephen.a.boyer@gmail.com>
2020-04-30 12:37:00 -0500

    reconfigured link for routes/tables diagram

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 
 ## Tables and Routes
-https://user-images.githubusercontent.com/61071438/80666718-b5381100-8a62-11ea-9587-c9a5571c9b15.png
+
+![alt text](https://i.imgur.com/Pi4BfQu.png )
--- models.py
+++ models.py
@@ -6,7 +6,13 @@ DATABASE = SqliteDatabase('users.sqlite')
 
 
 
+class User(UserMixin, Model):
+  username=CharField(unique=True)
+  email=CharField(unique=True)
+  password=CharField() 
 
+  class Meta: 
+    database=DATABASE
 
 
 
