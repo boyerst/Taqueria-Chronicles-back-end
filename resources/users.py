662b3df228f25e46da73ae7ee5f4a592164d79d6
Steve Boyer <stephen.a.boyer@gmail.com>
2020-04-30 14:31:01 -0500

    added users /register, /register route hitting

--- resources/users.py
+++ resources/users.py
@@ -1,11 +1,17 @@
 import models
-from flask import Blueprint
+from flask import Blueprint, request
 
 
 
 users = Blueprint('users', 'users')
 
-
+# TEST /users
 @users.route('/', methods=['GET'])
 def test_user_resource(): 
   return "user resource working" 
+
+# REGISTER /users/register
+@users.route('/register', methods=['POST'])
+def register():
+  print(request.get_json())
+  return "check terminal" 
