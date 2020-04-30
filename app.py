495087e4bccc562824b874a94b4dcd3eba6fffb7
Steve Boyer <stephen.a.boyer@gmail.com>
2020-04-30 13:59:27 -0500

    installed and imported CORS

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from resources.taquerias import taquerias
 
 import models
 
+from flask_cors import CORS
+
+from flask_login import LoginManager
+
 
 DEBUG=True
 PORT=8000
@@ -14,6 +18,7 @@ app = Flask(__name__)
 
 app.register_blueprint(taquerias, url_prefix='/api/v1/taquerias')
 
+CORS(taquerias, origins=['http://localhost:3000'], supports_credentials=True)
 
 # TEST
 @app.route('/')
