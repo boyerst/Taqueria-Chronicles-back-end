466a34aa87f09f1090db51938dfb719d5a45ea37
Steve Boyer <stephen.a.boyer@gmail.com>
2020-04-30 18:36:36 -0500

    user can update taqueria name

--- resources/taquerias.py
+++ resources/taquerias.py
@@ -45,10 +45,51 @@ def create_taqueria():
   ), 201
   
 
-  # DESTROY /taquerias/id
+# DESTROY /taquerias/id
 @taquerias.route('/<id>', methods=['DELETE'])
 def delete_taqueria(id):
   delete=models.Taqueria.delete().where(models.Taqueria.id == id)
-  num_rows=delete.execute()
-  print(num_rows)
-  return "check terminal"
+  num_of_rows=delete.execute()
+  print(num_of_rows)
+  return jsonify(
+    data={},
+    message="You have deleted {} taqueria with id {}".format(num_of_rows, id),
+    status=200
+  ), 200
+
+
+
+# UPDATE /taquerias/id
+@taquerias.route('/<id>', methods=['PUT'])
+def update_taqueria(id):
+  payload=request.get_json()
+  taqueria_to_update=models.Taqueria.get_by_id(id)
+  if taqueria_to_update.patron_id.id==current_user.id:
+    if 'name' in payload:
+      taqueria_to_update.name=payload['name'] 
+  
+    taqueria_to_update.save()
+    updated_taq_dict=model_to_dict(taqueria_to_update)
+    updated_taq_dict['patron_id'].pop('password')
+    return jsonify(
+      data=updated_taq_dict,
+      message=f"You have updated the taqueria with id {id}",
+      status=200
+    ), 200
+  else:
+    return jsonify(
+    data={
+      'error': '403 Forbidden'
+    },
+    message="patron_id does not match taqueria id. You can only edit taquerias that you chronicled.",
+    status=403
+    ), 403
+
+
+
+
+    taqueria_to_update.save()
+
+
+
+
